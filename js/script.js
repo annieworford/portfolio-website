19a8832ffdc5f26ccca635242121ea314f311fe4
annieworford <annieworford@gmail.com>
2021-01-17 14:22:42 -0700

    Fix hamburger menu toggle

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-console.log("Hi! Welcome to my portfolio site");
+console.log('Hi! Welcome to my portfolio site')
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-  x.className += ' responsive';
-} else {
-  x.className = 'navtoggle';
-}
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
 }
